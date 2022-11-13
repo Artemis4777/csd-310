2126d2a554f0d446dd565f34948135b81ae739f7
Artemis4777 <cberger0354@gmail.com>
2022-11-13 11:01:22 -0500

    module 12 final commit

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -6,13 +6,13 @@
 */
 
 -- drop test user if exists 
-DROP USER IF EXISTS 'whatabook_user'@'localhost';
+DROP USER IF EXISTS 'root'@'localhost';
 
--- create whatabook_user and grant them all privileges to the whatabook database 
-CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+-- create root and grant them all privileges to the whatabook database 
+CREATE USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
 
--- grant all privileges to the whatabook database to user whatabook_user on localhost 
-GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+-- grant all privileges to the whatabook database to user root on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'root'@'localhost';
 
 -- drop contstraints if they exist
 ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
